07b83b87113614d0b5df64453bd1539d2869fb6f
rodrigo <rodrigo_rmartins@outlook.com>
2021-04-12 20:33:41 -0400

    adding news list to the main activity

--- app/src/main/java/com/example/millennialnews/MainActivity.java
+++ app/src/main/java/com/example/millennialnews/MainActivity.java
@@ -1,18 +1,24 @@
 package com.example.millennialnews;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnSearch;
     private EditText etSearch;
+    private RecyclerView rvNews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
         btnSearch = findViewById(R.id.btnSearch);
         etSearch = findViewById(R.id.etSearch);
+
+        initRecyclerView();
     }
 
     @Override
@@ -52,5 +60,47 @@ public class MainActivity extends AppCompatActivity {
     private void openSettingsActivity(MenuItem item) {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
+    }
+
+    private void initRecyclerView() {
+        List<News> newsList = new ArrayList<>();
+        newsList.add(new News(
+                "MobileSyrup",
+                "Brad Bennett",
+                "Everything we know about Tesla’s Solar Roof tiles coming to Canada",
+                "Elon Musk’s much-hyped Tesla Solar panels are expected to arrive in Canada this year, but many specifications still remain elusive. With that in mind, below is everything we know about the solar panels so far.",
+                "2021-04-12T22:38:12Z",
+                "https://mobilesyrup.com/wp-content/uploads/2020/09/tesla-solar-roof-scaled.jpg"
+        ));
+        newsList.add(new News(
+                "MobileSyrup",
+                "Brad Bennett",
+                "Everything we know about Tesla’s Solar Roof tiles coming to Canada",
+                "Elon Musk’s much-hyped Tesla Solar panels are expected to arrive in Canada this year, but many specifications still remain elusive. With that in mind, below is everything we know about the solar panels so far.",
+                "2021-04-12T22:38:12Z",
+                "https://mobilesyrup.com/wp-content/uploads/2020/09/tesla-solar-roof-scaled.jpg"
+        ));
+        newsList.add(new News(
+                "MobileSyrup",
+                "Brad Bennett",
+                "Everything we know about Tesla’s Solar Roof tiles coming to Canada",
+                "Elon Musk’s much-hyped Tesla Solar panels are expected to arrive in Canada this year, but many specifications still remain elusive. With that in mind, below is everything we know about the solar panels so far.",
+                "2021-04-12T22:38:12Z",
+                "https://mobilesyrup.com/wp-content/uploads/2020/09/tesla-solar-roof-scaled.jpg"
+        ));
+        newsList.add(new News(
+                "MobileSyrup",
+                "Brad Bennett",
+                "Everything we know about Tesla’s Solar Roof tiles coming to Canada",
+                "Elon Musk’s much-hyped Tesla Solar panels are expected to arrive in Canada this year, but many specifications still remain elusive. With that in mind, below is everything we know about the solar panels so far.",
+                "2021-04-12T22:38:12Z",
+                "https://mobilesyrup.com/wp-content/uploads/2020/09/tesla-solar-roof-scaled.jpg"
+        ));
+
+        Log.d("MAIN_ACTIVITY", "initRecyclerView: init recyclerview");
+        rvNews = findViewById(R.id.rvNews);
+        NewsAdapter newsAdapter = new NewsAdapter(newsList);
+        rvNews.setAdapter(newsAdapter);
+        rvNews.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/com/example/millennialnews/NewsAdapter.java
@@ -0,0 +1,68 @@
+package com.example.millennialnews;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder> {
+
+    private final List<News> newsList;
+
+    public NewsAdapter(List<News> newsList) {
+        this.newsList = newsList;
+    }
+
+    @NonNull
+    @Override
+    public NewsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_item, parent, false);
+        return new NewsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NewsViewHolder holder, int position) {
+        holder.tvNewsTitle.setText(newsList.get(position).getTitle());
+        holder.tvNewsAuthor.setText(String.format(
+                "%s: { %s }",
+                newsList.get(position).getSource(),
+                newsList.get(position).getAuthor()));
+        holder.tvNewsDate.setText(newsList.get(position).getDate());
+        holder.tvNewsDescription.setText(newsList.get(position).getDescription());
+//        holder.ivNewsImage. setText(newsList.get(position).getImage());
+
+        holder.newsItemLayout.setOnClickListener(v ->
+                Toast.makeText(v.getContext(), newsList.get(position).getTitle(), Toast.LENGTH_SHORT).show());
+    }
+
+    @Override
+    public int getItemCount() {
+        return newsList.size();
+    }
+
+    public static class NewsViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvNewsTitle;
+        TextView tvNewsAuthor;
+        TextView tvNewsDate;
+        TextView tvNewsDescription;
+        ImageView ivNewsImage;
+        ConstraintLayout newsItemLayout;
+
+        public NewsViewHolder(View itemView) {
+            super(itemView);
+            tvNewsTitle = itemView.findViewById(R.id.tvNewsTitle);
+            tvNewsAuthor = itemView.findViewById(R.id.tvNewsAuthor);
+            tvNewsDate = itemView.findViewById(R.id.tvNewsDate);
+            tvNewsDescription = itemView.findViewById(R.id.tvNewsDescription);
+            ivNewsImage = itemView.findViewById(R.id.ivNewsImage);
+            newsItemLayout = itemView.findViewById(R.id.newsItemLayout);
+        }
+    }
+}
