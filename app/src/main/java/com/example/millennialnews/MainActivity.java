3167a534529497e10abb5e8de48fd449d9aee294
Caragine <ac83779p@pace.edu>
2021-04-13 12:05:15 -0400

    getNews and getFreshNews methods

---
+++ app/src/main/java/com/example/millennialnews/MainActivity.java
@@ -0,0 +1,218 @@
+package com.example.millennialnews;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.kwabenaberko.newsapilib.NewsApiClient;
+import com.kwabenaberko.newsapilib.models.Source;
+import com.kwabenaberko.newsapilib.models.request.EverythingRequest;
+import com.kwabenaberko.newsapilib.models.request.TopHeadlinesRequest;
+import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnSearch;
+    private EditText etSearch;
+    private RecyclerView rvNews;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSearch = findViewById(R.id.btnSearch);
+        etSearch = findViewById(R.id.etSearch);
+
+        getFreshNews();
+        //initRecyclerView();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.menuSettings) {
+            openSettingsActivity(item);
+            return true;
+        }
+        if (id == R.id.menuLogIn) {
+            openLogInActivity(item);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void openLogInActivity(MenuItem item) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    private void openSettingsActivity(MenuItem item) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    public void getFreshNews() {
+        List<NewsArticle> articleList = new ArrayList<NewsArticle>();
+
+        NewsApiClient newsApiClient = new NewsApiClient("8b88ab2e81df4547abfc23f6fc69311c");
+
+        newsApiClient.getTopHeadlines(
+                new TopHeadlinesRequest.Builder()
+                        .country("us")
+                        .build(),
+                new NewsApiClient.ArticlesResponseCallback() {
+                    @Override
+                    public void onSuccess(ArticleResponse response) {
+
+                        for (int i = 0; i < response.getArticles().size(); i++) {
+
+                            Source source = response.getArticles().get(i).getSource();
+                            String sourceName = source.getName();
+                            String author = response.getArticles().get(i).getAuthor();
+                            String title = response.getArticles().get(i).getTitle();
+                            String description = response.getArticles().get(i).getDescription();
+                            String date = response.getArticles().get(i).getPublishedAt();
+                            String image = response.getArticles().get(i).getUrlToImage();
+
+                            NewsArticle article = new NewsArticle(source, author, title, description, date, image);
+                            articleList.add(article);
+                            //System.out.println(response.getArticles().get(i).getTitle());
+                            //System.out.println(response.getArticles().get(i).getAuthor());
+                            //System.out.println(response.getArticles().get(i).getDescription());
+                            //System.out.println(response.getArticles().get(i).getPublishedAt());
+                            System.out.println(article.toString());
+                            //Log.i("articlecount", String.valueOf(articleList.size()));
+                            //Log.i("TEST", "SUCCESSFULLY ADDED TO LIST");
+                        }
+                        //Log.i("articlecount", String.valueOf(articleList.size()));
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        System.out.println(throwable.getMessage());
+                    }
+
+                });
+
+        System.out.println("TEST");
+        Log.i("MAIN_ACTIVITY", "initRecyclerView: init recyclerview");
+        }
+
+
+
+    public void getNews(View view) {
+        String queryString = etSearch.getText().toString();
+
+        List<NewsArticle> articleList = new ArrayList<NewsArticle>();
+
+        NewsApiClient newsApiClient = new NewsApiClient("8b88ab2e81df4547abfc23f6fc69311c");
+
+        newsApiClient.getEverything(
+                new EverythingRequest.Builder()
+                        .q(queryString)
+                        .sortBy("popularity")
+                        .build(),
+                new NewsApiClient.ArticlesResponseCallback() {
+                    @Override
+                    public void onSuccess(ArticleResponse response) {
+
+                        for (int i = 0; i < response.getArticles().size(); i++) {
+
+                            Source source = response.getArticles().get(i).getSource();
+                            String sourceName = source.getName();
+                            String author = response.getArticles().get(i).getAuthor();
+                            String title = response.getArticles().get(i).getTitle();
+                            String description = response.getArticles().get(i).getDescription();
+                            String date = response.getArticles().get(i).getPublishedAt();
+                            String image = response.getArticles().get(i).getUrlToImage();
+
+                            NewsArticle article = new NewsArticle(source, author, title, description, date, image);
+                            articleList.add(article);
+                            //System.out.println(response.getArticles().get(i).getTitle());
+                            //System.out.println(response.getArticles().get(i).getAuthor());
+                            //System.out.println(response.getArticles().get(i).getDescription());
+                            //System.out.println(response.getArticles().get(i).getPublishedAt());
+                            System.out.println(article.toString());
+                            Log.i("TEST", "SUCCESSFULLY ADDED TO LIST");
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        System.out.println(throwable.getMessage());
+                    }
+                }
+        );
+
+
+
+        //return articleList;
+    }
+
+
+
+  /*  private void initRecyclerView() {
+
+        List<NewsArticle> newsList = new ArrayList<>();
+
+       newsList.add(new NewsArticle(
+                ,
+                "Brad Bennett",
+                "Everything we know about Tesla’s Solar Roof tiles coming to Canada",
+                "Elon Musk’s much-hyped Tesla Solar panels are expected to arrive in Canada this year, but many specifications still remain elusive. With that in mind, below is everything we know about the solar panels so far.",
+                "2021-04-12T22:38:12Z",
+                "https://mobilesyrup.com/wp-content/uploads/2020/09/tesla-solar-roof-scaled.jpg"
+        ));
+        newsList.add(new NewsArticle(
+                "MobileSyrup",
+                "Brad Bennett",
+                "Everything we know about Tesla’s Solar Roof tiles coming to Canada",
+                "Elon Musk’s much-hyped Tesla Solar panels are expected to arrive in Canada this year, but many specifications still remain elusive. With that in mind, below is everything we know about the solar panels so far.",
+                "2021-04-12T22:38:12Z",
+                "https://mobilesyrup.com/wp-content/uploads/2020/09/tesla-solar-roof-scaled.jpg"
+        ));
+        newsList.add(new NewsArticle(
+                "MobileSyrup",
+                "Brad Bennett",
+                "Everything we know about Tesla’s Solar Roof tiles coming to Canada",
+                "Elon Musk’s much-hyped Tesla Solar panels are expected to arrive in Canada this year, but many specifications still remain elusive. With that in mind, below is everything we know about the solar panels so far.",
+                "2021-04-12T22:38:12Z",
+                "https://mobilesyrup.com/wp-content/uploads/2020/09/tesla-solar-roof-scaled.jpg"
+        ));
+        newsList.add(new NewsArticle(
+                "MobileSyrup",
+                "Brad Bennett",
+                "Everything we know about Tesla’s Solar Roof tiles coming to Canada",
+                "Elon Musk’s much-hyped Tesla Solar panels are expected to arrive in Canada this year, but many specifications still remain elusive. With that in mind, below is everything we know about the solar panels so far.",
+                "2021-04-12T22:38:12Z",
+                "https://mobilesyrup.com/wp-content/uploads/2020/09/tesla-solar-roof-scaled.jpg"
+        ));
+
+        Log.d("MAIN_ACTIVITY", "initRecyclerView: init recyclerview");
+        rvNews = findViewById(R.id.rvNews);
+        NewsAdapter newsAdapter = new NewsAdapter(newsList);
+        rvNews.setAdapter(newsAdapter);
+        rvNews.setLayoutManager(new LinearLayoutManager(this));
+    }
+*/
+}
---
+++ app/src/main/java/com/example/millennialnews/NewsArticle.java
@@ -0,0 +1,90 @@
+package com.example.millennialnews;
+
+import com.kwabenaberko.newsapilib.models.Source;
+
+public class NewsArticle {
+
+    private Source source;
+    private String author;
+    private String title;
+    private String description;
+    private String date;
+    private String image;
+
+    public NewsArticle(
+            Source source,
+            String author,
+            String title,
+            String description,
+            String date,
+            String image
+    ) {
+        this.source = source;
+        this.author = author;
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.image = image;
+    }
+
+    public Source getSource() {
+        return source;
+    }
+
+    public void setSource(Source source) {
+        this.source = source;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+
+    @Override
+    public String toString() {
+        return "News {" +
+                "source='" + source.getName() + '\'' +
+                ", author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", date='" + date + '\'' +
+                ", image='" + image + '\'' +
+                '}';
+    }
+}
